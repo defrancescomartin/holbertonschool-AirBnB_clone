70b126509624b8c51aafce0c3a234cb6a49c2c70
MateoArbini <mateoarbini1998@gmail.com>
2022-06-27 12:09:59 -0700

    file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+'''Write a class FileStorage that serializes instances to a JSON file
+and deserializes JSON file to instances
+'''
+
+class FileStorage():
+    '''creating class'''
+
