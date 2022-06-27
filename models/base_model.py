2208e6bfe9b0bd6cbc15ecd3fa972af25cd8c2da
MateoArbini <mateoarbini1998@gmail.com>
2022-06-27 08:52:16 -0700

    base_model - task3

--- models/base_model.py
+++ models/base_model.py
@@ -27,6 +27,6 @@ class BaseModel():
         '''return dict containing all key/value of __dict__ of an instance'''
         dictionary = self.__dict__.copy()
         dictionary["__class__"] = self.__class__.__name__
-        dictionary["created_at"] = self.created_at.isoformat()
         dictionary["updated_at"] = self.updated_at.isoformat()
+        dictionary["created_at"] = self.created_at.isoformat()
         return dictionary
