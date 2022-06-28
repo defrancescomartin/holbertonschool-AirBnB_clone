a024c93d0946d770c3068572812856586941948d
MateoArbini <mateoarbini1998@gmail.com>
2022-06-28 07:10:45 -0700

    basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -31,6 +31,7 @@ class BaseModel():
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
+            models.storage.new(self)
 
     def __str__(self):
         '''string representation'''
@@ -39,6 +40,7 @@ class BaseModel():
     def save(self):
         '''update attribute updated_at with current datetime'''
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         '''return dict containing all key/value of __dict__ of an instance'''
