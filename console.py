4021d979be9b6362daaa8243191a8d0d408087a3
defrancescomartin <defrancescoma@gmail.com>
2022-06-28 18:31:51 -0300

    task 6 cmd working

--- console.py
+++ console.py
@@ -6,3 +6,29 @@ import models
 from models.base_model import BaseModel
 
 class HBNBCommand(cmd.Cmd):
+    '''command interpreter'''
+
+    prompt = '(hbnb) '
+
+    def do_quit(self, arg):
+        '''to exit the program'''
+        return True
+
+    def do_EOF(self, arg):
+        '''exit the program'''
+        return True
+
+    def help_quit(self):
+        '''updated and documented help'''
+        print('Quit command to exit the program')
+
+    def help_EOF(self):
+        '''updated and documented help'''
+        print('EOF commando to exit')
+
+    def emptyline(self):
+        '''empty line + ENTER shouldn't execute anything'''
+        pass
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
