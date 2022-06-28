15693e8fce70f6625bf7c93db68b3d84feb01d02
MateoArbini <mateoarbini1998@gmail.com>
2022-06-28 07:27:33 -0700

    base and init

--- models/base_model.py
+++ models/base_model.py
@@ -12,7 +12,7 @@ Now it’s time to re-create an instance with this dictionary representation.
 import models
 import uuid
 from datetime import datetime
-
+from models import storage
 
 class BaseModel():
     '''class Base'''
@@ -31,7 +31,7 @@ class BaseModel():
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
-            models.storage.new(self)
+            storage.new()
 
     def __str__(self):
         '''string representation'''
@@ -40,7 +40,7 @@ class BaseModel():
     def save(self):
         '''update attribute updated_at with current datetime'''
         self.updated_at = datetime.now()
-        models.storage.save()
+        storage.save()
 
     def to_dict(self):
         '''return dict containing all key/value of __dict__ of an instance'''
