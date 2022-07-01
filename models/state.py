58b83c1de086ba990a1f05aa1bbb6c83f3472180
defrancescomartin <defrancescoma@gmail.com>
2022-07-01 10:34:12 -0300

    class State with name as empty string

---
+++ models/state.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""class State that inherit from BaseModel"""
+
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+
+    '''Public class attributes: ''' 
+    '''name: empty string'''
+
+    name = ""
