436e06f4ab4ee64162415077985e0c2f8822d69f
defrancescomartin <defrancescoma@gmail.com>
2022-07-01 10:51:23 -0300

    created Place class

---
+++ models/place.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+'''class Place that inherits from BaseModel'''
+
+from models.base_model import BaseModel
+
+
+class Place(BaseModel):
+
+    '''Public class attributes: '''
+    '''city_id, user_id, name, description: empty strings'''
+    '''number_rooms, number_bathrooms, max_guests, price_by_night: integer-0'''
+    '''latitude, longitude: float-0.0'''
+    '''amenity_ids: list of strings'''
+
+    city_id = ""
+    user_id = ""
+    name = ""
+    description = ""
+    number_rooms = 0
+    number_bathrooms = 0
+    max_guests = 0
+    price_by_night = 0
+    latitude = 0.0
+    longitude = 0.0
+    amenity_ids = []
