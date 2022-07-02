0939ebba2fdf296941e4ba1aadcb1765367dc1e5
MateoArbini <mateoarbini1998@gmail.com>
2022-07-02 06:47:45 -0700

    fixed attr max_guest

--- models/place.py
+++ models/place.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-
 '''class Place that inherits from BaseModel'''
+
 
 from models.base_model import BaseModel
 
@@ -19,7 +19,7 @@ class Place(BaseModel):
     description = ""
     number_rooms = 0
     number_bathrooms = 0
-    max_guests = 0
+    max_guest = 0
     price_by_night = 0
     latitude = 0.0
     longitude = 0.0
